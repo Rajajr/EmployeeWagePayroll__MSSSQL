fe2f34cc015f220d7bd00862f9f2190c5cd54d7a
Rajajr <rajjr011@gmail.com>
2022-11-02 06:55:04 +0700

    UC-2 Create a Table

--- Employee Payrolls.sql
+++ Employee Payrolls.sql
@@ -1,9 +1,5 @@
 --UC1-create a datatbase
 create database Payroll_Service;
-
-
-
-
-
-
+--UC2-Create table
+create table EmployeePayroll(ID int identity(1,1)primary key,Empname varchar(200),Salary bigint,Startdate date)
 
