59e856e3b272afae6df51c1d61e3e929e686ee06
Rajajr <rajjr011@gmail.com>
2022-11-02 07:31:04 +0700

    UC-4 Retrive All Records

--- Employee Payrolls.sql
+++ Employee Payrolls.sql
@@ -6,8 +6,10 @@ create table EmployeePayroll(ID int identity(1,1)primary key,Empname varchar(200
 
 --UC3-Insert Records
 insert into EmployeePayroll(Empname,Salary,Salary) values('Raj',20000,'2020-02-03');
-insert into EmployeePayroll(Empname,Salary,Salary) values('Ramya',20000,'2020-01-01');
-insert into EmployeePayroll(Empname,Salary,Salary) values('Sam',20000,'2020-01-02');
-insert into EmployeePayroll(Empname,Salary,Salary) values('Daniel',20000,'2020-01-05');
-insert into EmployeePayroll(Empname,Salary,Salary) values('Sara',20000,'2020-01-06');
+insert into EmployeePayroll(Empname,Salary,Salary) values('Ramya',22000,'2020-01-01');
+insert into EmployeePayroll(Empname,Salary,Salary) values('Sam',12000,'2020-01-02');
+insert into EmployeePayroll(Empname,Salary,Salary) values('Daniel',18000,'2020-01-05');
+insert into EmployeePayroll(Empname,Salary,Salary) values('Sara',25000,'2020-01-06');
 
+--UC4-Retrive All Records
+select *from EmployeePayroll
